264781559187d164061b56dfd1ae215898d5f317
Kexue Wang <kexue.wang@grabtaxi.com>
2018-06-17 10:25:51 +0800

    add read module

--- log_process.go
+++ log_process.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"time"
 )
 
 type Reader interface {
-	Read(rc chan string)
+	Read(rc chan []byte)
 }
 
 type Writer interface {
@@ -18,10 +21,30 @@ type ReadFromFile struct {
 	path string //读取文件的路径
 }
 
-func (r *ReadFromFile) Read(rc chan string) {
+func (r *ReadFromFile) Read(rc chan []byte) {
 	// 读取模块
-	line := "message"
-	rc <- line
+
+	// 打开文件
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file error:%s", err.Error()))
+	}
+
+	// 从文件末尾开始逐行读取文件内容
+	f.Seek(0, 2)
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadBytes error %s", err.Error()))
+		}
+		rc <- line[:len(line) - 1]
+	}
+
 }
 
 type WriteToInfluxDB struct {
@@ -30,11 +53,13 @@ type WriteToInfluxDB struct {
 
 func (w *WriteToInfluxDB) Write(wc chan string) {
 	// 写入模块
-	fmt.Println(<-wc)
+	for value:= range wc {
+		fmt.Println(value)
+	}
 }
 
 type LogProcess struct {
-	rc    chan string
+	rc    chan []byte
 	wc    chan string
 	read  Reader
 	write Writer
@@ -42,22 +67,23 @@ type LogProcess struct {
 
 func (l *LogProcess) Process() {
 	// 解析模块
-	data := <-l.rc
-	l.wc <- strings.ToUpper(data)
+	for value := range l.rc {
+		l.wc <- strings.ToUpper(string(value))
+	}
 }
 
 func main() {
-	r := &ReadFromFile{path: "/temp/access.log"}
+	r := &ReadFromFile{path: "./access.log"}
 	w := &WriteToInfluxDB{influxDBDsn: "username&password..."}
 	lp := &LogProcess{
-		rc:    make(chan string),
-		wc:    make(chan string),
-		read:  r,
-		write: w,
+		make(chan []byte),
+		make(chan string),
+		r,
+		w,
 	}
 	go lp.read.Read(lp.rc)
 	go lp.Process()
 	go lp.write.Write(lp.wc)
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(30 * time.Second)
 }
